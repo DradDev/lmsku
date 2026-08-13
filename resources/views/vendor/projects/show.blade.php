c5ec48f785b464933525b650e8f0f5f41a4530b7
Dapin118 <davinraditya@students.undip.ac.id>
2026-08-13 20:04:21 +0700

    ux(vendor): remove target=_blank from vendor student portfolio links to navigate in the same tab

--- resources/views/vendor/projects/show.blade.php
+++ resources/views/vendor/projects/show.blade.php
@@ -426,7 +426,7 @@
                                         </div>
 
                                         <div class="flex items-center gap-1.5 pt-1 border-white/10">
-                                            <a href="{{ route('vendor.students.portfolio', $recStudent) }}" target="_blank"
+                                            <a href="{{ route('vendor.students.portfolio', $recStudent) }}"
                                                class="flex-1 text-center py-1 px-2 bg-white/20 hover:bg-white/30 text-white font-semibold text-[10px] rounded-md transition">
                                                 Portofolio
                                             </a>
